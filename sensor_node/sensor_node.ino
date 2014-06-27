3bb9e12fc309308b9b2c1336c7737f5775b20a1c
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2014-06-26 23:20:51 -0400

    Start testing by borrowing code from AdaFruit's DHTtester sketch

--- sensor_node/sensor_node.ino
+++ sensor_node/sensor_node.ino
@@ -1,7 +1,39 @@
+/*
+
+Hungarian Notation Prefixes:
+- DP: Digtal Pin
+- C: Constant (The value is one of several constants like DHT11)
+
+*/
+
+#include "DHT.h"
+
+// Temperature sensor configuration
+#define DP_DHTPIN 3
+#define C_DHTTYPE DHT11
+//#define C_DHTTYPE DHT22
+DHT dht(DP_DHTPIN, C_DHTTYPE);
+
+// -- For now, build on code from AdaFruit's DHTtester sketch to
+//    make sure I've wired everything correctly.
 void setup() {
 
+  Serial.begin(9600);
+  Serial.println("Connection established");
 }
 
 void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
   
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT");
+  } else {
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C %\t");
+  }
 }

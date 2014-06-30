b44d2ea48e0b6eeabb9d339d87c43854ae355feb
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2014-06-30 03:01:22 -0400

    Add a fan-controller rule to prevent the corner from getting musty

--- control_node/rules.py
+++ control_node/rules.py
@@ -7,6 +7,9 @@
 __author__ = "Stephan Sokolow (deitarion/SSokolow)"
 __license__ = "GNU GPL 2 or later"
 
+import logging
+log = logging.getLogger(__name__)
+
 from actions import call_x10
 
 class WasteNoLight(object):
@@ -25,6 +28,37 @@ class WasteNoLight(object):
         if corner_brightness > 750:
             call_x10(self.lights, False)
 
+class NoDampCorners(object):
+    """Use fans to relocate humid air before it can make corners musty."""
+
+    min_diff = 5.0     # When the fans should turn off
+    max_diff = 10.0    # When the fans should turn on
+    fan_state = None
+
+    def __init__(self, baseline_sensor_id, corner_sensor_id, fans):
+        self.baseline_sensor = baseline_sensor_id
+        self.corner_sensor = corner_sensor_id
+        self.fans = fans
+
+    def __call__(self, model):
+        rh_vals = {x: model.get(x, {}).get('humidity') for x in
+                (self.baseline_sensor, self.corner_sensor)}
+
+        for name, value in rh_vals.items():
+            if value is None:
+                log.error("Could not read humidity from %s", name)
+                return
+
+        humid_diff = abs(rh_vals.values()[0] - rh_vals.values()[1])
+
+        if humid_diff > self.max_diff and not self.fan_state is True:
+            call_x10(self.fans, True)
+            self.fan_state = True
+        elif humid_diff < self.min_diff and not self.fan_state is False:
+            call_x10(self.fans, False)
+            self.fan_state = False
+
 RULES = [
-    WasteNoLight('corner', [1, 2, 6])
+    WasteNoLight('corner', [1, 2, 6]),
+    NoDampCorners('desktop', 'corner', 3)
 ]
